f8b950345c695242901dd3bc48758e0943a5a365
barriern <nicolas.barrier@ird.fr>
2020-11-23 07:38:02 +0100

    Adding py file connected to notebook (store only inputs and can be edited using sphinx or vscode)

---
+++ apecosm/report_template.py
@@ -0,0 +1,32 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light,md
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.7.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # Apecosm report
+#
+# ## Total biomass
+
+# + tags=["remove_output"]
+import numpy as np
+x = np.arange(10)
+print(x)
+# -
+
+import numpy as np
+x = np.arange(10)
+print(x)
+
+# + tags=["remove_input"]
+x = 1
+print(x)
