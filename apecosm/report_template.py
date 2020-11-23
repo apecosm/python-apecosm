07c3d06eb194cb9c627a532baf6e54293aff47da
barriern <nicolas.barrier@ird.fr>
2020-11-23 11:24:20 +0100

    Adding reporting methods

--- apecosm/report_template.py
+++ apecosm/report_template.py
@@ -14,19 +14,24 @@
 # ---
 
 # # Apecosm report
-#
-# ## Total biomass
-
-# + tags=["remove_output"]
-import numpy as np
-x = np.arange(10)
-print(x)
-# -
-
-import numpy as np
-x = np.arange(10)
-print(x)
 
 # + tags=["remove_input"]
-x = 1
-print(x)
+import apecosm
+import matplotlib.pyplot as plt
+plt.rcParams['image.cmap'] = 'jet'
+
+input_dir = '/home/barrier/Codes/apecosm/git-apecosm-config/gyre_multicom/output_apecosm'
+input_mesh = '/home/barrier/Codes/apecosm/git-apecosm-config/gyre/mesh_mask.nc'
+# -
+
+# ## Time-series
+
+apecosm.plot_report_ts(input_dir, input_mesh)
+
+# ## Maps
+
+apecosm.plot_report_map(input_dir, input_mesh)
+
+# ## Size-spectras
+
+apecosm.plot_report_size_spectra(input_dir, input_mesh)
