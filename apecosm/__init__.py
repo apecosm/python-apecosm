bf44a377681d68370ef15cf1febb8406bb9fc5fa
barriern <nicolas.barrier@ird.fr>
2020-11-23 11:18:39 +0100

    Adding plot_report functions + extraction of constant field

--- apecosm/__init__.py
+++ apecosm/__init__.py
@@ -22,10 +22,11 @@ __author_email__ = "nicolas.barrier@ird.fr"
 
 from .conf import read_config
 from .domains import inpolygon, plot_domains
-from .extract import extract_ltl_data, extract_time_means, extract_oope_data
+from .extract import extract_ltl_data, extract_time_means, extract_oope_data, extract_apecosm_constants
 from .misc import find_percentile, compute_daylength, extract_community_names, size_to_weight, weight_to_size
 from .netcdf import rebuild_restart
 from .size_spectra import compute_spectra_ltl, plot_oope_spectra, set_plot_lim
-from .mplot import plot_oope_map, plot_season_oope 
+from .mplot import plot_oope_map, plot_season_oope
+from .report import  plot_report_map, plot_report_ts, plot_report_size_spectra
 from .grid import extract_weight_grid, read_ape_grid, plot_grid_nemo_ape, partial_step_ape
 from .habitat import get_tcor, compute_o2, compute_tpref, compute_tlim, compute_lightpref
