b99fadddff5ab3901980ca039041abe30f9399a4
barriern <nicolas.barrier@ird.fr>
2022-03-31 18:05:12 +0200

    adding size_cumprop to diag

--- apecosm/diags.py
+++ apecosm/diags.py
@@ -1,7 +1,7 @@
 import xarray as xr
 from .extract import extract_oope_data
 
-def compute_size_cumprop(data, mesh, const, maskdom=None):
+def compute_size_cumprop(mesh, data, const, maskdom=None):
     
     output = extract_oope_data(data, mesh, const, maskdom, use_wstep=True)
     output = output.cumsum(dim='w') / output.sum(dim='w')
